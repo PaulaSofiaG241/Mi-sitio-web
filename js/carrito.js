be2bd07521fe85a1c11f395abdd0e45a12354134
PaulaSofiaG241 <125401748+PaulaSofiaG241@users.noreply.github.com>
2023-10-17 14:28:52 -0500

    Add files via upload

--- js/carrito.js
+++ js/carrito.js
@@ -1,20 +1,44 @@
 let boxPlus = document.getElementById("boxPlus");
 let boxUnidades = document.getElementById("boxUnidades");
 let boxMinus = document.getElementById("boxMinus");
+let subTotal = document.getElementById("subTotal");
+const valorUnidad=52000;
+const valorUnidadStr = Intl.NumberFormat("de-De").format(valorUnidad);
 
+document.getElementById("precioUnidad").innerHTML = "$"+ valorUnidadStr;
 
 
 boxPlus.addEventListener("click", function(){
     let auxUnidades = parseInt(boxUnidades.innerHTML);
     if(auxUnidades < 10){
-        document.getElementById("boxUnidades").innerHTML =( auxUnidades + 1).toString();
+        boxUnidades.innerHTML =( auxUnidades + 1).toString();
+        let valorSubtotal = parseInt(boxUnidades.innerHTML)* valorUnidad;
+        subTotal.innerHTML = Intl.NumberFormat("de-De").format(valorSubtotal);
     };
+    if(boxUnidades.innerHTML == 1){
+        document.getElementById("cantProductos").innerHTML =
+        boxUnidades.innerHTML + "Producto";
+    }
+    else{
+        document.getElementById("cantProductos").innerHTML = boxUnidades.innerHTML +"Productos";
+    }
 });
 
 boxMinus.addEventListener("click", function(){
     let auxUnidades = parseInt(boxUnidades.innerHTML);
     if(auxUnidades > 0){
-        document.getElementById("boxUnidades").innerHTML =( auxUnidades - 1).toString();
+        boxUnidades.innerHTML =( auxUnidades - 1).toString();
+        let valorSubtotal = parseInt(boxUnidades.innerHTML)* valorUnidad;
+        subTotal.innerHTML = Intl.NumberFormat("de-De").format(valorSubtotal);
     };
+    if(boxUnidades.innerHTML == 1){
+        document.getElementById("cantProductos").innerHTML =
+        boxUnidades.innerHTML + "Producto";
+    }
+    else{
+        document.getElementById("cantProductos").innerHTML = boxUnidades.innerHTML +"Productos";
+    }
 });
+
+
 
